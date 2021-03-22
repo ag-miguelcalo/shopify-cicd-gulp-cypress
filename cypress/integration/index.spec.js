72a71ff166d274964e96704e65da8ae246753791
Miguel Calo <miguel.calo@acidgreen.com.au>
2021-03-22 12:28:58 +0800

    feature: announcement and basic test

---
+++ cypress/integration/index.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+context('Index Page', () => {
+  before(() => {
+    cy.visit('/');
+    cy.get('input[type=password]').type('qqq111');
+    cy.get('button[type=submit]').click();
+  });
+
+  describe('Announcement', () => {
+    it('Should have announcement element', () => {
+      cy.get('.ag-announcement h1 a').should('have.text', 'My First Announcement!');
+    });
+  });
+});
